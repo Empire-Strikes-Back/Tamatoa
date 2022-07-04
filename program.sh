0dea833ed396ae317c40b46d6481528fe523cee8
Tamatoa <Tamatoa@Realm-of-Monstors>
2022-07-04 12:38:13 +0300

    my release version is fully defined by commit hash :Hammond seriously, insects are my genuine phobia - what's yours? :Clarkson you know - manual labour

--- program.sh
+++ program.sh
@@ -14,24 +14,13 @@ main(){
     -M -m Tamatoa.main
 }
 
-tag(){
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
-  TAG="$COMMIT_COUNT-$COMMIT_HASH"
-  git tag $TAG $COMMIT_HASH
-  echo $COMMIT_HASH
-  echo $TAG
-}
-
 jar(){
 
   rm -rf out/*.jar out/classes
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
   clojure \
     -X:Genie Genie.core/process \
     :main-ns Tamatoa.main \
-    :filename "\"out/Tamatoa-$COMMIT_COUNT-$COMMIT_HASH.jar\"" \
+    :filename "\"out/Tamatoa-$(git rev-parse --short HEAD).jar\"" \
     :paths '["src"]'
 }
 
