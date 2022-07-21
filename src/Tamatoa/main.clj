924e301a8903bbb5c2749aad982c44ab06bf13a0
Tamatoa <Tamatoa@Realm-of-Monstors>
2022-07-21 16:56:57 +0300

    our wine is wine - because raisins have too much sugar - which feeds yeast and candida
    :Kristen-Bell mama, it's time i told you the truth about Ricky
    :Jimmy-Kimmel i saw Frozen last night - and it was shit

--- src/Tamatoa/main.clj
+++ src/Tamatoa/main.clj
@@ -19,7 +19,7 @@
    [Tamatoa.B12]
    [Tamatoa.salt]
    [Tamatoa.bread]
-   [Tamatoa.raisins])
+   [Tamatoa.wine])
   (:import
    (javax.swing JFrame WindowConstants JPanel JScrollPane JTextArea BoxLayout JEditorPane ScrollPaneConstants SwingUtilities JDialog)
    (javax.swing JMenu JMenuItem JMenuBar KeyStroke JOptionPane JToolBar JButton JToggleButton JSplitPane JLabel JTextPane JTextField JTable JTabbedPane)
@@ -69,7 +69,7 @@
    '[Tamatoa.B12]
    '[Tamatoa.salt]
    '[Tamatoa.bread]
-   '[Tamatoa.raisins]
+   '[Tamatoa.wine]
    '[Tamatoa.main]
    :reload))
 
@@ -195,7 +195,7 @@
             jpanel-B12 (JPanel.)
             jpanel-salt (JPanel.)
             jpanel-bread (JPanel.)
-            jpanel-raisins (JPanel.)]
+            jpanel-wine (JPanel.)]
 
         (doto jtabbed-pane
           (.setTabLayoutPolicy JTabbedPane/SCROLL_TAB_LAYOUT)
@@ -203,7 +203,7 @@
           (.addTab "B12" jpanel-B12)
           (.addTab "salt" jpanel-salt)
           (.addTab "bread" jpanel-bread)
-          (.addTab "raisins" jpanel-raisins)
+          (.addTab "wine" jpanel-wine)
           (.setSelectedComponent jpanel-bread))
 
         (Tamatoa.bread/process {:jpanel-tab jpanel-bread
